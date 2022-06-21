975f9539d0d2c2a9362c2d5b0f9ad0c8f54dcca1
Rafael Borges <rafaelborges.av@gmail.com>
2022-06-21 17:25:54 -0300

    nav

--- components/MainPincipal.vue
+++ components/MainPincipal.vue
@@ -1,20 +1,11 @@
 <template>
   <main class="mx-8">
-    <SectionHero />
-    <SectionCards />
-    <AboutUs />
-    <NossosParceiros />
+    <h4>teste hahaha</h4>
   </main>
 </template>
 
 <script>
-import SectionCards from "./SectionCards.vue";
-import SectionHero from "./SectionHero.vue";
-import AboutUs from "./AboutUs.vue";
-import NossosParceiros from "./NossosParceiros.vue";
-export default {
-  components: { SectionCards, SectionHero, AboutUs, NossosParceiros },
-};
+export default {};
 </script>
 
 <style scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,21 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "cinza-claro": "#D4D4D4",
+        "cinza-escuro": "#777777",
+        "preto-fosco": "#222222",
+        "azul-1": "#0C4A6E",
+        "azul-2": "#006490",
+        "azul-3": "#0EA5E9",
+        "azul-4": "#111827",
+        "azul-5": "#E0F2FE",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
