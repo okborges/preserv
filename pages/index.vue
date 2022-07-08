036f554bce829a46c40f8950ee3088a5601dcd7f
Rafael Borges <rafaelborges.av@gmail.com>
2022-07-08 12:02:21 -0300

    section sobre nos

--- pages/index.vue
+++ pages/index.vue
@@ -4,15 +4,17 @@
     <SectionUm />
     <SectionDois />
     <ParceirosClientes />
+    <SobreNos />
   </div>
 </template>
 
 <script>
 import SectionDois from "~/components/SectionDois.vue";
 import ParceirosClientes from "~/components/ParceirosClientes.vue";
+import SobreNos from "~/components/SobreNos.vue";
 export default {
   name: "IndexPage",
-  components: { SectionDois, ParceirosClientes },
+  components: { SectionDois, ParceirosClientes, SobreNos },
 };
 </script>
 
